3f9c5a10d2c672e01f0c5955d8bca1ba4c26e858
shalev ofir <50710665+ofirent@users.noreply.github.com>
2025-04-12 22:13:52 +0000

    dd

--- backend/routes/budget.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getBudget, setBudget } = require("../controllers/budgetController");
-
-router.get("/", getBudget);
-router.post("/", setBudget);
-
-module.exports = router;
